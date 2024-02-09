b44b4885c1828f7044508e54388ad2f4f26fa0f4
happyntec <tec@happyn.cn>
2024-02-09 11:50:34 +0800

    for #3

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import sys
 from happynserver.view.ui_trayicon import UITrayIcon
 from happynserver.view.ui_main_window import Ui_HappynServerWindow
 from PySide2.QtWidgets import QApplication, QFrame
+from PySide2.QtCore import QEvent
 from PySide2 import QtCore
 import platform
 from ctypes import windll
+
 
 class HappynetUIMainWindow(QFrame, Ui_HappynServerWindow):
     def __init__(self):
@@ -13,20 +15,23 @@ class HappynetUIMainWindow(QFrame, Ui_HappynServerWindow):
         # 将UI界面布局到Demo上；
         self.setupUi(self)
 
-    def changeEvent(self, event):
-        if event.type() == 8:  # 8 表示窗口最小化事件
-            self.hide_taskbar_button()
+        # 创建托盘图标实例
+        self.tray_icon = UITrayIcon(self)
+        # self.tray_icon.show()
 
-    def hide_taskbar_button(self):
-        if platform.system() == 'Windows':
-            hwnd = self.winId()
-            windll.user32.ShowWindow(hwnd, 0x8)  # 0x8 表示SW_HIDE
+    def changeEvent(self, event):
+        if event.type() == QEvent.WindowStateChange:
+            if self.isMinimized():
+                self.hide()  # 在窗口最小化时隐藏窗口
+                self.tray_icon.show()  # 显示托盘图标
+            else:
+                super().changeEvent(event)  # 处理其他状态变化
+
 
 if __name__ == "__main__":
     QtCore.QCoreApplication.setAttribute(QtCore.Qt.ApplicationAttribute.AA_EnableHighDpiScaling)
     app = QApplication(sys.argv)
-    main_window = HappynetUIMainWindow()
-    tray_icon = UITrayIcon(app, main_window=main_window)
-    tray_icon.show()
-    tray_icon.on_show_main_window()
+    mainWindow = HappynetUIMainWindow()
+    mainWindow.tray_icon = UITrayIcon(mainWindow)
+    mainWindow.show()
     sys.exit(app.exec_())
